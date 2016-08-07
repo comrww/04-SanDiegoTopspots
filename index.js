5650240eaa4c7f354f9eda726ac3f1a237eb5da2
Ryan Wilson <comrww@gmail.com>
2016-08-06 17:04:21 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+		$.getJSON("https://raw.githubusercontent.com/OriginCodeAcademy/2016-CW-FallCohort/master/Projects/Week%201/04-SanDiegoTopSpots/topspots.json", function(data) {
+			console.log("it worked");
+		});
+});
